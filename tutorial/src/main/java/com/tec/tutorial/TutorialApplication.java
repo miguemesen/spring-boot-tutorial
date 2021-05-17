add048464b6890d793f0e6d924ad0c8e5b30d413
miguemesen <miguemesen12@gmail.com>
2021-05-17 17:49:25 -0600

    Hello world

--- tutorial/src/main/java/com/tec/tutorial/TutorialApplication.java
+++ tutorial/src/main/java/com/tec/tutorial/TutorialApplication.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-
+@RestController
 public class TutorialApplication {
 
 	public static void main(String[] args) {
@@ -14,5 +14,8 @@ public class TutorialApplication {
 	}
 
 
-
+	@GetMapping
+	public String hello(){
+		return "Hello World";
+	}
 }
