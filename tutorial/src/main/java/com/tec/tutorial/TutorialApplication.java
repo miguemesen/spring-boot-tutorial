0427e4189f69c47658cefc00fc1a4ffec9bef0fb
miguemesen <miguemesen12@gmail.com>
2021-05-17 16:59:12 -0600

    image

--- tutorial/src/main/java/com/tec/tutorial/TutorialApplication.java
+++ tutorial/src/main/java/com/tec/tutorial/TutorialApplication.java
@@ -2,12 +2,17 @@ package com.tec.tutorial;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+
 public class TutorialApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TutorialApplication.class, args);
 	}
+
+
 
 }
